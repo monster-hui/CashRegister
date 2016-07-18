46675c732165b08cc8d68d0bbcf8a8bd3ac4c35b
wxh <373000945@qq.com>
2016-07-18 20:02:46 +0800

    ConstructFrame

---
+++ data/goods.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/7/18.
+ */
+var goods=[
+    {"barcode": "ITEM000000","name": "可口可乐","unit": "瓶","category": "食品","subCategory": "碳酸饮料","price": "3.00"},
+    {"barcode": "ITEM000001","name": "百事可乐","unit": "瓶","category": "食品","subCategory": "碳酸饮料","price": "3.00"},
+    {"barcode": "ITEM000002","name": "非常可乐","unit": "瓶","category": "食品","subCategory": "碳酸饮料","price": "3.00"},
+    {"barcode": "ITEM000003","name": "脉动","unit": "瓶","category": "食品","subCategory": "功能性饮料","price": "4.00"},
+    {"barcode": "ITEM000004","name": "萨姆奶茶","unit": "瓶","category": "食品","subCategory": "茶饮料","price": "4.00"},
+    {"barcode": "ITEM000005","name": "冰红茶","unit": "瓶","category": "食品","subCategory": "茶饮料","price": "5.00"},
+    {"barcode": "ITEM000006","name": "羽毛球","unit": "个","category": "运动类","subCategory": "球类","price": "4.00"},
+    {"barcode": "ITEM000007","name": "苹果","unit": "斤","category": "食品","subCategory": "水果","price": "5.50"},
+    {"barcode": "ITEM000008","name": "篮球","unit": "个","category": "运动类","subCategory": "球类","price": "98.00"}
+]
