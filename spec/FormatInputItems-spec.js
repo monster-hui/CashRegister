d442e2b053d76f778d935c568dde966d65fe6088
wangzhangjun <695331215@qq.com>
2016-07-19 15:45:30 +0800

    add the FormatInputItem of tht TDD

--- spec/FormatInputItems-spec.js
+++ spec/FormatInputItems-spec.js
@@ -1,16 +1,12 @@
-describe('printing',function(){
- var Print;
- beforeEach(function(){
-  Print = require('../src/printing.js');
- });
+describe('formatInputItems',function(){
  it('get items and counts from input data',function(){
   var data= ['ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000003','ITEM000003-2','ITEM000005','ITEM000005','ITEM000005'];
-   var output = Print(data);
+   var output = formatInputItems(data);
    var expectedOutput = [
     {"ITEM000001":5},
     {'ITEM000003':3},
     {'ITEM000005':3}
    ];
-   expect(output).toEqual({expectedOutput});
+   expect(output).toEqual(expectedOutput);
  });
 });
