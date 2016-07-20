fb9d94baab339a0aa8c14a82e2c6c5b5eaf5d58a
wangzhangjun <695331215@qq.com>
2016-07-20 14:21:15 +0800

    为formatInputItems-spec.js添加了几个测试用例

--- spec/formatInputItems-spec.js
+++ spec/formatInputItems-spec.js
@@ -1,12 +1,38 @@
-describe('formatInputItems',function(){
- it('get items and counts from input data',function(){
-  var data= ['ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000003','ITEM000003-2','ITEM000005','ITEM000005','ITEM000005'];
+describe('  formatInputItems',function(){
+ it('get items and counts from input data just have item,no "-"',function(){
+  var data= ['ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000001','ITEM000003','ITEM000005','ITEM000005','ITEM000005'];
    var output = formatInputItems(data);
    var expectedOutput = [
-    {"ITEM000001":5},
-    {'ITEM000003':3},
-    {'ITEM000005':3}
+		{"ITEM000001":5},
+		{'ITEM000003':1},
+		{'ITEM000005':3}
    ];
    expect(output).toEqual(expectedOutput);
+ });
+ it('get items and counts from input data when have "-";eg:"ITEM000003-2"', function(){
+	var data = ['ITEM000001','ITEM000001','ITEM000003-2','ITEM000005'];
+	var output = formatInputItems(data);
+	var expectedOutput = [
+		{"ITEM000001":2},
+		{'ITEM000003':2},
+		{'ITEM000005':1}
+	];
+	expect(output).toEqual(expectedOutput);
+ });
+  it('get items and counts from input data when have -;eg:"ITEM000003-2,ITEM000003 "', function(){
+	var data = ['ITEM000001','ITEM000001','ITEM000003-2','ITEM000003','ITEM000005'];
+	var output = formatInputItems(data);
+	var expectedOutput = [
+		{"ITEM000001":2},
+		{'ITEM000003':3},
+		{'ITEM000005':1}
+	];
+	expect(output).toEqual(expectedOutput);
+ });
+   it('get items and counts from input data when data is empty', function(){
+	var data = [];
+	var output = formatInputItems(data);
+	var expectedOutput = [];
+	expect(output).toEqual(expectedOutput);
  });
 });
